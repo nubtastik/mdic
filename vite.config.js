3007f856aa146d7bf2a7ebc0c6b5a9fdbb19cf53
Tom Bostick <nubtastik@gmail.com>
2026-02-03 17:26:41 -0600

    Fix base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MDIC/',     // MUST match the repo name exactly (MDIC)
+  base: '/mdic/',
   build: {
-    outDir: 'docs',   // Build output for GitHub Pages
+    outDir: 'docs',
   },
 })
